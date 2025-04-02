262e6e7bae609dbe4e4427154d8510688503c335
Roman Sushko <7en.roman+git@gmail.com>
2025-04-01 21:03:58 -0400

    Homework 7.3. Search for a substring

---
+++ hw7_3.py
@@ -0,0 +1,16 @@
+# ДЗ 7.3. Пошук підрядка
+
+def second_index(text, any_str):
+    first_index = text.find(any_str)
+    result = text.find(any_str, first_index + 1)
+
+    if first_index == -1 or result == -1:
+        return None
+    else:
+        return result
+
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')
