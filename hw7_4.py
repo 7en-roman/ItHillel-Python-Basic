f5d6b15fbab62732948ae1046c46dcfa7494664c
Roman Sushko <7en.roman+git@gmail.com>
2025-04-01 21:04:32 -0400

    Homework 7.4. Search for common elements

---
+++ hw7_4.py
@@ -0,0 +1,15 @@
+# ДЗ 7.4. Пошук спільних елементів
+
+def common_elements():
+    list1 = []
+    list2 = []
+    for el in range(0, 100):
+        if el % 3 == 0:
+            list1.append(el)
+    for el in range(0, 100):
+        if el % 5 == 0:
+            list2.append(el)
+    return set(list1).intersection(set(list2))
+
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+print('ОК')
