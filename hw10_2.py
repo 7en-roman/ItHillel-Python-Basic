2cbebe7dcf0b4777efed129772147709c3dea587
Roman Sushko <7en.roman+git@gmail.com>
2025-04-01 21:07:31 -0400

    Homework 10.2. Find the first word

---
+++ hw10_2.py
@@ -0,0 +1,22 @@
+# ДЗ 10.2. Знайти перше слово
+
+import string
+
+def first_word(text):
+    """ Пошук першого слова """
+
+    for i in string.punctuation:
+        if i in text and i != "'":
+           text = text.replace(i, " ").strip()
+
+    words = text.split()
+
+    return str(words[0]) if words else None
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
